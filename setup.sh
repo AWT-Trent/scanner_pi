180650a09a6d409b1d69bb618b8e29c231e06e52
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-02-26 14:01:46 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,7 +5,7 @@ cp -r ../scanner_pi /usr/bin/scanner_pi
 cp ./scanner_pi_usb /usr/bin/scanner_pi_usb
 chmod +x /usr/bin/scanner_pi/scanner_pi_usb
 sed -i -e '$i \bash /usr/bin/scanner_pi_usb # libcomposite configuration\n' /etc/rc.local
-sed -i -e '$i \python /usr/bin/scanner_pi/scanner_flask.py --host $_IP\n' /etc/rc.local
+sed -i -e '$i \python /usr/bin/scanner_pi/scanner_flask.py --host "$_IP"\n' /etc/rc.local
 apt-get install pip -y
 pip install flask
 reboot
