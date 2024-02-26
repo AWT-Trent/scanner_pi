01e3158288f67f59812d9fb8fd163af083d3a7ed
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-02-26 12:32:43 -0500

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 echo "dtoverlay=dwc2" | sudo tee -a /boot/config.txt
 echo "dwc2" | sudo tee -a /etc/modules
 sudo echo "libcomposite" | sudo tee -a /etc/modules
+cp ../scanner_pi /usr/bin/scanner_pi
 cp ./scanner_pi_usb /usr/bin/scanner_pi_usb
-cp ./scanner_flask.py /usr/bin/scanner_flask.py
-sudo chmod +x /usr/bin/scanner_pi_usb
+sudo chmod +x /usr/bin/scanner_pi/scanner_pi_usb
 sed -i -e '$i \/usr/bin/scanner_pi_usb # libcomposite configuration\n' /etc/rc.local
-sed -i -e '$i \python /usr/bin/scanner_flask.py --host $_IP\n' /etc/rc.local
+sed -i -e '$i \python /usr/bin/scanner_pi/scanner_flask.py --host $_IP\n' /etc/rc.local
 apt-get install pip git -y
 pip install flask
 reboot
