cb9c3d0d6cb66d66bd9729398caed3fd69c273f7
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-02-27 14:37:38 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,10 +1,11 @@
 rm /usr/bin/scanner_pi_usb
 git clone https://github.com/AWT-Trent/scanner_pi.git
 rm ./update.sh
-chmod +x ./update.sh
+chmod +x ./scanner_pi/update.sh
 cp ./scanner_pi/update.sh ./update.sh
 cp ./scanner_pi_usb /usr/bin/scanner_pi_usb
-mv -f ./scanner_pi /usr/bin/
+cp -r ./scanner_pi /usr/bin/scanner_pi
 chmod +x /usr/bin/scanner_pi/scanner_pi_usb
+rm -r ./scanner_pi
 pip install -r ./scanner_pi/requirements.txt
 reboot () { echo 'Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; }
