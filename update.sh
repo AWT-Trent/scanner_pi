0632bb760fea86243dfe06b86ad5efa8c6d15cac
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-02-27 14:33:29 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -7,4 +7,4 @@ cp ./scanner_pi_usb /usr/bin/scanner_pi_usb
 mv -f ./scanner_pi /usr/bin/
 chmod +x /usr/bin/scanner_pi/scanner_pi_usb
 pip install -r ./scanner_pi/requirements.txt
-reboot
+reboot () { echo 'Reboot? (y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot; }
