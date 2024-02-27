193e98f6e9eb8d314c785620e0d1bd7b27c943e3
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-02-27 16:09:21 -0500

    Update update.sh

--- update.sh
+++ update.sh
@@ -1,8 +1,9 @@
 rm /usr/bin/scanner_pi_usb
+cp -r /usr/bin/scanner_pi/config.ini ./config.ini
 git clone https://github.com/AWT-Trent/scanner_pi.git
 cp -r ./scanner_pi ../
 cp ./scanner_pi/scanner_pi_usb /usr/bin/scanner_pi_usb
-cp -r ./scanner_pi /usr/bin/scanner_pi
+cp -r ./scanner_pi /usr/bin/
 chmod +x /usr/bin/scanner_pi/scanner_pi_usb
 pip install -r ./scanner_pi/requirements.txt
 rm -r ./scanner_pi
