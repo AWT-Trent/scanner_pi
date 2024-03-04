e9c241681fbbed33bf43f8894cb9cd7406000ab4
AWT-Trent <160764468+AWT-Trent@users.noreply.github.com>
2024-03-04 15:03:18 -0500

    added timer logic

--- scanner_flask.py
+++ scanner_flask.py
@@ -4,8 +4,14 @@ import time
 import sys
 from configparser import ConfigParser
 import os
-from datetime import datetime
+from datetime import datetime,timedelta
 import random
+from random import randint
+from threading import Thread
+import logging
+log = logging.getLogger('werkzeug')
+log.setLevel(logging.ERROR)
+
 
 config = ConfigParser()
 
@@ -20,16 +26,15 @@ def init_settings():
         config.set('main', 'random_scans_per_day_settings', '{"min":"1","max":"10"}')
         config.set('main', 'random_interval_settings', '{"min":"1","max":"9999"}')
         config.set('main','items','')
+        config.set('main','last_run_time','')
+        config.set('main','next_run_time','')
 
         with open('config.ini', 'w') as f:
             config.write(f)
         
         config.read('config.ini') 
            
-try:    
-    config.read('config.ini')  
-except:
-    init_settings()
+
     
 args = [x.upper() for x in sys.argv]
 print(args)
@@ -78,6 +83,45 @@ def update_log():
         logs = ''
     return(logs)
 
+def run():
+    print('task run')
+
+def update():
+    config.read('config.ini')
+    last_run = config.get('main','last_run_time')
+    next_run = config.get('main','next_run_time')
+    
+    if not last_run:
+        last_run = datetime.now()
+        
+    if not next_run or datetime.strptime(next_run, '%Y-%m-%d %H:%M:%S.%f') < datetime.now():
+        run()
+        
+        
+        interval = json.loads(config.get('main', 'random_interval_settings'))
+        interval['min'] = int(interval['min'])
+        interval['max'] = int(interval['max'])
+        interval = randint(interval['min'],interval['max'])
+        print(interval)
+        next_run_time = datetime.now() + timedelta(seconds=interval)
+        print(f'Next run at {next_run_time}')
+        config.set('main','last_run_time',str(datetime.now()))
+        config.set('main','next_run_time',next_run_time.strftime('%Y-%m-%d %H:%M:%S.%f'))
+        with open('config.ini', 'w') as f:
+            config.write(f)
+        write_to_log(f'Task ran. Next runtime {next_run_time}')
+    
+def task_daemon():
+    try:    
+        config.read('config.ini')  
+    except:
+        init_settings()
+    while 1:
+        update()
+        #print('ran')
+        time.sleep(.1)
+        
+        
 @app.route('/')
 def index():
     global logs
@@ -173,6 +217,7 @@ def save_settings():
     save_upcs_to_select_settings(upcs_to_select_min)
     save_random_scans_per_day_settings(random_scans_per_day_min, random_scans_per_day_max)
     save_random_interval_settings(random_interval_min, random_interval_max)
+    write_to_log('Saved Settings')
     return jsonify({'message': 'Settings saved successfully'})
 
 def save_upcs_to_select_settings(min_val):
@@ -228,6 +273,10 @@ def test():
     return jsonify({'message': 'Settings saved successfully'})
 
 if __name__ == '__main__':
+    
+    thread = Thread(target=task_daemon,daemon=True)
+    thread.start()
+    
     if host:
         app.run(debug=True, host=host)
     else:
